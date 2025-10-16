013b7813a56844d075df1f65954cd60bff81d721
Rishab <rishabmallick20004@gmail.com>
2025-10-17 03:32:35 +0530

    connect to mongo atlas

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);   // return object
+        console.log(`\n MongoDb connected !! DB HOST ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection Error", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/db.js";
+dotenv.config({ path: './.env' });
+/*  1st approach to connect DB 
+import express from "express";
+const app = express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error", () => {
+            console.log("Database is connected , but express app is not working");
+            throw error;
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listing on ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
+*/
+
+// 2nd approach to connect DB , best way:
+connectDB();
+
